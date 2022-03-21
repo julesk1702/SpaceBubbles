9ffa1b039f7dfb5e07efa1b34db3d25a40cf9e85
julesk1702 <89418218+julesk1702@users.noreply.github.com>
2022-03-22 00:32:31 +0100

    LevelSelector aangepast

--- scenes/LevelSelector.java
+++ scenes/LevelSelector.java
@@ -5,11 +5,22 @@ import com.github.hanyaeger.SpaceBubbles.buttons.LevelButton;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.StaticScene;
 
+import java.util.ArrayList;
+
 public class LevelSelector extends StaticScene {
     private BubbleShooter bubbleShooter;
+    private String level;
+    private int levelId;
+    private int xPos;
+
+    ArrayList<String> levels = new ArrayList<String>();
+
 
     public LevelSelector(BubbleShooter bubbleShooter){
         this.bubbleShooter = bubbleShooter;
+        levels.add("Level 1");
+        levels.add("Level 2");
+        levels.add("Level 3");
     }
 
     @Override
@@ -20,17 +31,15 @@ public class LevelSelector extends StaticScene {
 
     @Override
     public void setupEntities() {
-        var level1Button = new LevelButton(
-                new Coordinate2D(100, 100), bubbleShooter, 2, "Level 1"
-        );
-        var level2Button = new LevelButton(
-                new Coordinate2D(300, 100), bubbleShooter, 3, "Level 2"
-        );
-        var level3Button = new LevelButton(
-                new Coordinate2D(500, 100), bubbleShooter, 3, "Level 3"
-        );
-        addEntity(level1Button);
-        addEntity(level2Button);
-        addEntity(level3Button);
+        for(int i = 0; i < levels.size(); i++){
+            level = levels.get(i);
+            levelId++;
+            xPos += 150;
+
+            var levelButton = new LevelButton(
+                    new Coordinate2D(xPos, 100), bubbleShooter, levelId, level
+            );
+            addEntity(levelButton);
+        }
     }
 }
