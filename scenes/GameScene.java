63a2efcdf77ea759ccb71d3fcc03bf2477f0891e
julesk1702 <89418218+julesk1702@users.noreply.github.com>
2022-03-30 00:21:55 +0200

    meerdere rijen planeten aangemaakt

--- scenes/GameScene.java
+++ scenes/GameScene.java
@@ -33,33 +33,88 @@ public class GameScene extends DynamicScene {
     @Override
     public void setupEntities() {
         var xPos = 30;
+        var yPos = 50;
+
+
         for (var location : this.planeten) {
             xPos += 40;
         }
 
+
+
         for (int i = 0; i < 10; i++) {
-            var planeet = getPlaneet(xPos, i);
+            var planeet = getPlaneet(xPos, i, yPos);
             planeten.add(planeet);
+
+            addEntity(planeet);
+        }
+        for (int i = 0; i < 10; i++) {
+            var planeet = getPlaneet(xPos, i, 100);
+            planeten.add(planeet);
+
             addEntity(planeet);
         }
 
+        for (int i = 0; i < 10; i++) {
+            var planeet = getPlaneet(xPos, i, 150);
+            planeten.add(planeet);
 
+            addEntity(planeet);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            var planeet = getPlaneet(xPos, i, 200);
+            planeten.add(planeet);
+
+            addEntity(planeet);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            var planeet = getPlaneet(xPos, i, 250);
+            planeten.add(planeet);
+
+            addEntity(planeet);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            var planeet = getPlaneet(xPos, i, 300);
+            planeten.add(planeet);
+
+            addEntity(planeet);
+        }
     }
-// Heb het zo aangepast dat je maar een class hebt voor planeten in te spawnen en in deze switch dus de afbeelding meegeeft als variabel.
-    private Planeet getPlaneet(int xPos, int i) {
-        int type = ThreadLocalRandom.current().nextInt(0, 3);
+
+
+    // Heb het zo aangepast dat je maar een class hebt voor planeten in te spawnen en in deze switch dus de afbeelding meegeeft als variabel.
+    private Planeet getPlaneet(int xPos, int i, int yPos) {
+        int type = ThreadLocalRandom.current().nextInt(0, 8);
         return switch (type) {
             case 0 -> new soortPlaneet(
-                    new Coordinate2D(xPos + i * 50, 50), "sprites/mars.png"
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/mercury.png"
             );
             case 1 -> new soortPlaneet(
-                    new Coordinate2D(xPos + i * 50, 50), "sprites/earth.png"
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/venus.png"
             );
             case 2 -> new soortPlaneet(
-                    new Coordinate2D(xPos + i * 50, 50), "sprites/venus.png"
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/earth.png"
+            );
+            case 3 -> new soortPlaneet(
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/mars.png"
+            );
+            case 4 -> new soortPlaneet(
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/jupiter.png"
+            );
+            case 5 -> new soortPlaneet(
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/saturn.png"
+            );
+            case 6 -> new soortPlaneet(
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/uranus.png"
+            );
+            case 7 -> new soortPlaneet(
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/neptune.png"
             );
             default -> new soortPlaneet(
-                    new Coordinate2D(xPos + i * 50, 50), "mars.png"
+                    new Coordinate2D(xPos + i * 50, yPos), "sprites/mars.png"
             );
         };
     }
